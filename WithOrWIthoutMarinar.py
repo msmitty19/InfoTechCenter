1fbf17d5f8452cf3b80fba30b2f959cf602fdbac
Smith <mgavin52828@gmail.com>
2026-01-14 12:21:26 -0500

    While Loop... 95% Stable - Doing Research

--- WithOrWIthoutMarinar.py
+++ WithOrWIthoutMarinar.py
@@ -1,4 +1,24 @@
 #Welcome Branch
+
+#Libraries Imported Here
+import sys
+import time
+
 print("Welcome Branch - Developer: Matthew Smith")
 print("\nWelcom to InfoTechCenter V.1.0")
-print("\nInfoTechCenter is Booting Up...")
+
+
+x = 0 
+ellipsis = 0
+
+while x != 20: 
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS is Booting Up" + "." * ellipsis)
+    ellipsis += 1 
+    sys.stdout.write("\r"+ ellipsisMessage) 
+    sys.stdout.flush()
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\nOperating System Booted Up - Access Granted")
